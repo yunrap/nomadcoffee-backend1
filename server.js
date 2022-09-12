aab7c1ffcc17b281a23ab4bf3ec38fcd3aa3fd46
yunrap <dbsdpwl7@gmail.com>
2022-09-12 18:51:14 +0900

    인스타그램프로젝트 기본설정

---
+++ server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const { ApolloServer } = require("apollo-server");
+import schema from "./schema";
+
+const server = new ApolloServer({
+  schema,
+});
+
+const PORT = process.env.PORT;
+
+server
+  .listen(PORT)
+  .then(() => console.log(`Server is running  http://localhost:${PORT}/"`));
